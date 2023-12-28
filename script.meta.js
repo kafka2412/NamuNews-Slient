22b14d82b1c2165214a43be5252047587cbd603c
Kafka2412 <152188547+kafka2412@users.noreply.github.com>
2023-12-28 11:19:10 +0900

    Create script.meta.js

---
+++ script.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         NamuNews Silent
+// @namespace    kafka2412
+// @version      0.0.1
+// @description  Remove comment section in NamuNews
+// @author       kafka2412
+// @supportURL   https://github.com/kafka2412/NamuNews-Slient/discussions
+// @downloadURL  https://github.com/kafka2412/NamuNews-Slient/releases/latest/download/NamuNewsSilent.user.js
+// @updateURL    https://github.com/kafka2412/NamuNews-Slient/releases/latest/download/NamuNewsSilent.meta.js
+// @match        https://namu.news/article/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
