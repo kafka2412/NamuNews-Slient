ec8ae4f1ae411550b6ccc72f3a24872dda630b27
Kafka2412 <152188547+kafka2412@users.noreply.github.com>
2023-12-28 14:49:09 +0900

    [1.0.0] Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NamuNews Silent
 // @namespace    kafka2412
-// @version      0.0.3
+// @version      1.0.0
 // @description  Remove comment section in NamuNews
 // @author       kafka2412
 // @supportURL   https://github.com/kafka2412/NamuNews-Slient/discussions
@@ -10,20 +10,28 @@
 // @match        https://namu.news/article/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-    
-    let timer = setInterval(function(){ 
-        const collection = document.getElementsByClassName("am7kwa-1");
-        // console.log(collection);
-        if (collection.length > 0) {
-            const parent = document.getElementsByClassName("am7kwa-0");
-            parent[0].remove();
-            // clearInterval(timer);
-        }
-        // console.log(collection);
-    },2000); //1000ms
 
+    // Referred https://stackoverflow.com/a/32338471
+    waitForKeyElements (
+        'div.am7kwa-0', processDivComments
+    );
+
+    waitForKeyElements (
+        'div.qsonk5-1', processDivCommentsNum
+    );
+    
+    function processDivComments (element) {
+        // console.log('catched!')
+        element.remove();
+    }
+
+    function processDivCommentsNum (element) {
+        element.remove();
+    }
 })();
